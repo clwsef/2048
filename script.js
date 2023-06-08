d22e91f54a12152cd01f74bc0c8b1f71b6802f2b
clwsef <135839448+clwsef@users.noreply.github.com>
2023-06-08 22:56:32 +0600

    Create board

---
+++ script.js
@@ -0,0 +1,26 @@
+let board
+let score=0
+let rows=4
+let columns=4
+window.onload=function(){
+    setGame()
+}
+function setGame(){
+    board=[
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]
+    ]
+    for (let r=0;r<rows;r++){
+        for (let c=0;c<columns;c++){
+            let tile=document.createElement('div')
+            tile.id=r.toString()+'-'+c.toString()
+            let num= board[r][c]
+            updateTile(tile,num)
+            document.getElementById('board').append(tile)
+        }
+    }
+    setTwo()
+    setTwo()
+}
