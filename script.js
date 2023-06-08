d16662d8a750567772a0c260b5b915d4fa2f4b29
clwsef <135839448+clwsef@users.noreply.github.com>
2023-06-08 23:05:26 +0600

    slideleft/right/up/down

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ function setGame(){
     setTwo()
     setTwo()
 }
+
+
 function updateTile(tile,num){
     tile.innerText=''
     tile.classList.value=''
@@ -34,6 +36,98 @@ function updateTile(tile,num){
             tile.classList.add('x'+num.toString())
         } else{
             tile.classList.add('x8192')
+        }
+    }
+}
+document.addEventListener('keyup',(e)=>{
+    if (e.code=='ArrowLeft'){
+        slideLeft()
+        setTwo()
+    }
+    else if (e.code=='ArrowRight'){
+        slideRight()
+        setTwo()
+    }
+    else if (e.code=='ArrowUp'){
+        slideUp()
+        setTwo()
+    }
+    else if (e.code=='ArrowDown'){
+        slideDown()
+        setTwo()
+    }
+    document.querySelector('#score').innerText=score
+})
+
+
+function slide(row){
+    row = filterZero(row) //[0,2,2,2]=>[2,2,2]
+    for (let i=0;i<row.length-1;i++){
+        if (row[i]==row[i+1]){
+            row[i]*=2
+            row[i+1]=0
+            score+=row[i]
+        } //[2,2,2]=>[4,0,2]
+    }
+    row=filterZero(row) //[4,2]
+    while (row.length<columns){
+        row.push(0)
+    } //[4,2,0,0]
+    return row
+}
+function slideLeft(){
+    for (let r=0;r<rows;r++){
+        let row=board[r]
+        row=slide(row)
+        board[r]=row
+        for (let c=0;c<columns;c++){
+            let tile=document.getElementById(r.toString()+'-'+c.toString())
+            let num=board[r][c]
+            updateTile(tile,num)
+        }
+    }
+}
+function slideRight(){
+    for (let r=0;r<rows;r++){
+        let row=board[r]
+        row.reverse()
+        row=slide(row)
+        row.reverse()
+        board[r]=row
+        for (let c=0;c<columns;c++){
+            let tile=document.getElementById(r.toString()+'-'+c.toString())
+            let num=board[r][c]
+            updateTile(tile,num)
+        }
+    }
+}
+function slideUp(){
+    for (let c=0;c<columns;c++){
+        let row=[board[0][c],board[1][c],board[2][c],board[3][c]]
+        row=slide(row)
+        // board[0][c] = row[0]
+        // board[1][c] = row[1]   //=board[r][c]=row[r]  
+        // board[2][c] = row[2]
+        // board[3][c] = row[3]
+        for (let r=0;r<rows;r++){
+            board[r][c]=row[r]  
+            let tile=document.getElementById(r.toString()+'-'+c.toString())
+            let num=board[r][c]
+            updateTile(tile,num)
+        }
+    }
+}
+function slideDown(){
+    for (let c=0;c<columns;c++){
+        let row=[board[0][c],board[1][c],board[2][c],board[3][c]]
+        row.reverse()
+        row=slide(row)
+        row.reverse()
+        for (let r=0;r<rows;r++){
+            board[r][c]=row[r]  
+            let tile=document.getElementById(r.toString()+'-'+c.toString())
+            let num=board[r][c]
+            updateTile(tile,num)
         }
     }
 }
